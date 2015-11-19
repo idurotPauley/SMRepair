4f2ade2b61339f2d825f6d1a0beb3de51a458d33
idurotPauley <p.idurot04@gmail.com>
2015-11-19 17:54:46 -0500

    update

--- simModel/RVPs.java
+++ simModel/RVPs.java
@@ -5,7 +5,7 @@ import cern.jet.random.engine.MersenneTwister;
 
 class RVPs 
 {
-	ModelName model; // for accessing the clock
+	SMRepair model; // for accessing the clock
     // Data Models - i.e. random veriate generators for distributions
 	// are created using Colt classes, define 
 	// reference variables here and create the objects in the
@@ -32,6 +32,20 @@ class RVPs
 	    // Note that interarrival time is added to current
 	    // clock value to get the next arrival time.
 	    return(nxtInterArr+model.getClock());
+	}
+	
+	//DuType1000CallArrival()
+	private int DuType1000CallArrival(){
+		if(0 <= model.getClock() < 60){
+			// return next arrival time with mean 60/7
+		}
+		else if(60 <= model.getClock() < 120){
+			//return next arriva
+		}
+	}
+	
+	private int DuType2000CallArrival(){
+		
 	}
 
 }
